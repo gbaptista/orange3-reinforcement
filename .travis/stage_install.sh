6998a0ada16574b63aa4e6da697ab96f1695cc9f
gbaptista <guilhermebaptistasilva@gmail.com>
2018-09-24 22:13:04 -0300

    fix travis

--- .travis/stage_install.sh
+++ .travis/stage_install.sh
@@ -1,6 +1,6 @@
 
 for script in \
-    install_orange.sh \
+    install_orange3_reinforcement.sh \
     install_pyqt.sh
 do
     foldable source $TRAVIS_BUILD_DIR/.travis/$script
--- .travis/stage_script.sh
+++ .travis/stage_script.sh
@@ -1,4 +1,5 @@
 # Screen must be 24bpp lest pyqt5 crashes, see pytest-dev/pytest-qt/35
 XVFBARGS="-screen 0 1280x1024x24"
 
-catchsegv xvfb-run -a -s "$XVFBARGS" pytest
+catchsegv xvfb-run -a -s "$XVFBARGS" pytest \
+  --cov reinforcement/widgets/ reinforcement/widgets/tests/
