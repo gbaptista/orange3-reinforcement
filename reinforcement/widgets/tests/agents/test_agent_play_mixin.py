d700febcaae8d897a30be0882ce7e431e25cd60e
gbaptista <guilhermebaptistasilva@gmail.com>
2018-09-25 23:21:52 -0300

    improve code coverage

--- reinforcement/widgets/tests/agents/test_agent_play_mixin.py
+++ reinforcement/widgets/tests/agents/test_agent_play_mixin.py
@@ -1,12 +1,57 @@
+from functools import partial
+
+import gym
+
+from Orange.widgets.utils.concurrent import ThreadExecutor
+
 from ...agents.agent_play_mixin import AgentPlayMixin
 
 
-def test_play():
-    class GenericAgent(AgentPlayMixin):
-        pass
+class GenericAgent(AgentPlayMixin):
+    enviroment_id = 'FrozenLake-v0'
 
+    enviroment = gym.make('FrozenLake-v0')
+
+    def play_action(self, _state):
+        return 0
+
+
+def test_play():
     generic_agent = GenericAgent()
 
+    generic_agent.playing = False
+
     assert not generic_agent.playing
+
+    generic_agent.play()
+
+    assert generic_agent.playing
+
+    generic_agent.playing = False
+
     assert generic_agent.episodes_interval == 0.0
     assert generic_agent.games_interval == 0.0
+
+
+def test_stop():
+    generic_agent = GenericAgent()
+
+    generic_agent.playing = True
+
+    assert generic_agent.playing
+
+    generic_agent.stop()
+
+    assert not generic_agent.playing
+
+
+def test_play_task():
+    generic_agent = GenericAgent()
+
+    state = 0
+
+    generic_agent.playing = False
+
+    generic_agent.play_task(state)
+
+    assert not generic_agent.playing
