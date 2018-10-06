f2ca56c273c8c54e46c3e61e1097b0dd5c5dd05f
gbaptista <guilhermebaptistasilva@gmail.com>
2018-10-05 21:57:39 -0300

    fix travis

--- .travis/install_openai_baselines.sh
+++ .travis/install_openai_baselines.sh
@@ -1,11 +1,13 @@
 OPENAI_BASELINES=$TRAVIS_BUILD_DIR/openai_baselines
 
-if [ ! "$(ls $if [ ! "$(ls $OPENAI_BASELINES)" ]; then)" ]; then
+if [ ! "$(ls $OPENAI_BASELINES)" ]; then
     mkdir -p $OPENAI_BASELINES
 
     cd $OPENAI_BASELINES
 
     wget -O baselines.zip https://github.com/openai/baselines/archive/master.zip
+
+    sudo apt-get install -y unzip
 
     unzip baselines.zip -d .
 fi  
